dd66465051ce2a12b1d4d09b1fcccda511e84f70
Rikkard29 <absolon.mm@gmail.com>
2022-03-03 22:06:32 +0100

    Extract method

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import SocketServer
 import socket
 import sipproxy as proxy
 
+def get_ip_address():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
+
 if __name__ == "__main__":    
     print('Starting sip proxy')
-    hostname = socket.gethostname()
-    ipaddress = socket.gethostbyname(hostname)
-    proxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress, proxy.PORT)
-    proxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress, proxy.PORT)
+    ip_address = get_ip_address()
+    proxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (ip_address, proxy.PORT)
+    proxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (ip_address, proxy.PORT)
     server = SocketServer.UDPServer((proxy.HOST, proxy.PORT), proxy.UDPHandler)
     print('Running...')
     server.serve_forever()
